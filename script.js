695f09af1037f074b26bcb93a946383b3fe713ae
arthur@aclee.fr <arthur@aclee.fr>
2023-03-02 10:28:48 +0100

    panier piano

---
+++ script.js
@@ -0,0 +1,110 @@
+
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 65 || event.key === "a") {
+        console.log("The 'A' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/A.mp3";
+        audio.play();
+
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 83 || event.key === "s") {
+        console.log("The 'S' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/S.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 68 || event.key === "d") {
+        console.log("The 'D' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/D.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 70 || event.key === "f") {
+        console.log("The 'F' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/F.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 71 || event.key === "g") {
+        console.log("The 'G' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/G.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 72 || event.key === "h") {
+        console.log("The 'H' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/H.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 74 || event.key === "j") {
+        console.log("The 'J' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/J.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 87 || event.key === "w") {
+        console.log("The 'W' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/W.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 69 || event.key === "e") {
+        console.log("The 'E' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/E.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 84 || event.key === "t") {
+        console.log("The 'T' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/T.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 89 || event.key === "y") {
+        console.log("The 'Y' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/Y.mp3";
+        audio.play();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 85 || event.key === "u") {
+        console.log("The 'U' key was pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/U.mp3";
+        audio.play();
+    }
+});
